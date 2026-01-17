7c79ca0abf59fc866441c96656e0265c8cf4a137
xiyii-star <1743623557@qq.com>
2026-01-18 00:56:14 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   trailingSlash: true,
   // 配置环境变量
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
   },
   // 跳过动态路由的构建时检查
   skipTrailingSlashRedirect: true,
